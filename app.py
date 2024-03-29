22a00f124c5b27f31fa8381696b40edf7845efc3
Adhivp <adhivp910@gmail.com>
2024-03-29 12:22:32 +0530

    Connected to flask

--- app.py
+++ app.py
@@ -59,6 +59,9 @@ def logout():
 def home():
     return render_template("index.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0", port=env.get("PORT", 3000))
